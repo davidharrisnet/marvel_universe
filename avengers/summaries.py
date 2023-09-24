5efd6d71d3d9e61eb2e068e68ec2e0684d503b17
David Harris <davidharrisnet@gmail.com>
2023-09-24 17:30:05 -0400

    reworked selenium

--- avengers/summaries.py
+++ avengers/summaries.py
@@ -19,19 +19,16 @@ def valid_title(filename):
     return filename
 
 def click_link(driver, text):
-    element = WebDriverWait(driver, 10).until(
+    element = WebDriverWait(driver, 20).until(
         EC.element_to_be_clickable((By.LINK_TEXT, text))
     )
     element.click()
     return element
-
-def get_table(driver, class_name):
-        table = driver.find_element(By.CLASS_NAME, class_name)
-        return table.find_elements(By.TAG_NAME,"td")
      
 class Avengers:
-    def __init__(self, url, top_dir="issue_summaries_xmen"):
-        self.top_dir = top_dir
+    def __init__(self, url, output_dir):
+        self.output_dir = output_dir
+        os.makedirs(self.output_dir, exist_ok=True)
         self.url = url
         self.driver = self.get_driver()
         self.soup = self.open_page()
@@ -53,77 +50,88 @@ class Avengers:
         page_source = self.driver.page_source
         return BeautifulSoup(page_source, "html.parser")
 
-
-
+    def get_links(self):
+        table =  self.driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div[2]/div[4]/table")
+        return table.find_elements(By.TAG_NAME, "a")
+    
+    def get_link(self, index):
+        links = self.get_links()
+        return links[index]
+    
+    def click_all(self):
+        element = click_link(self.driver, '50')
+        for _ in range(4):
+            element.send_keys(Keys.ARROW_DOWN)
+        element.send_keys(Keys.ENTER)
+        apply_button =self.driver.find_element(By.ID, "edit-submit-summaries-for-title")
+        apply_button.click()
+            
     def get_summaries(self):
-        titles = self.soup.find_all("td", class_="views-field views-field-field-comic-titles")
+        #titles = self.soup.find_all("td", class_="views-field views-field-field-comic-titles")
+        #table = self.driver.find_element(By.CLASS_NAME,"views-table.cols-3")
+        #table = self.driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div[2]/div[4]/table")
+        #(By.XPATH,"/html/body/div[1]/div/div[1]/div[2]/div[4]/table")
+        links =  self.get_links()
         
         #rows = get_table(self.driver, "views-table cols-3")
-        offset = 107
-        for index, title in enumerate(titles[offset:]):
-            
-            link = title.find("a")
+        offset = 0
+        for index, _ in enumerate(links[offset:]):
+        
+            link = self.get_link(index +  offset)
             comic_title = link.text
+                
             print(f"{index + offset} {comic_title}")
-            if comic_title == "4":
-                continue
+    
             # create output directory for this summaries pages
             comic_title_dir = valid_title(comic_title)
-            title_dir = os.path.join(self.top_dir, comic_title_dir)
+            title_dir = os.path.join(self.output_dir, comic_title_dir)
             os.makedirs(title_dir, exist_ok=True)
 
             comic_title = comic_title.strip()
-            try:
-                click_link(self.driver, comic_title)
+            try:                
+                link.click()
             except Exception as e:
                 print(e)
+                link = self.get_link(index +  offset)
+                link.click()
             try:
                 click_link(self.driver, "Issue Summaries")
-            except Exception as e:
-                print(e)
-            #url = self.driver.current_url
-            #self.driver.get(url)
-           
-    
-            element = click_link(self.driver, '50')
-            for _ in range(4):
-                element.send_keys(Keys.ARROW_DOWN)
-            element.send_keys(Keys.ENTER)
-            apply_button =self.driver.find_element(By.ID, "edit-submit-summaries-for-title")
-            apply_button.click()
-            #summary_url = self.driver.current_url
-              
-           
-           
+            except Exception as e:                
+                print("No Issue Summaries Tab")
+                self.driver.back() # back to all menu
+                continue
+  
+            self.click_all()               
             
-            #element = self.driver.find_element(By.ID, "edit-submit-summaries-for-title")
-            #element.click()
-                               
-            try:    
-                #soup = BeautifulSoup(requests.get(summary_url).content, "html.parser") 
-                title = Title(self.driver,self.soup, title_dir)
-                title.save_issues()
+            try:                  
+                title = Title(self.driver, title_dir)
+                title.save_issues()                
             except Exception as e:
                 print(e)
+                
+            self.driver.back() # back to all menu
             
 
 class Title():
-    def __init__(self, driver, soup, title_dir):
-        self.driver = driver        
-        self.soup = soup
+    def __init__(self, driver, title_dir):
+        self.driver = driver                
         self.story_object = {}
         self.title_dir = title_dir  
-              
-     
+        
+    def get_table(self):
+            table = self.driver.find_element(By.CLASS_NAME, "views-table")
+            return table.find_elements(By.TAG_NAME,"td")
+        
     def save_issues(self):       
-        summary_tds = get_table(self.driver, "views-table")
+        summary_tds = self.get_table()
+        
         self.summaries(summary_tds)
 
     def summaries(self, summary_tds):
-        for index, summary_td in enumerate(summary_tds):           
+        for index, _ in enumerate(summary_tds):           
             try:
                 if index %2 == 0:
-                    tds = get_table(self.driver, "views-table")
+                    tds = self.get_table()
                     link = tds[index].find_element(By.TAG_NAME, "a")
                     link.click()
                 else:
@@ -142,7 +150,7 @@ class Title():
                 
             except Exception as e:
                 print(e)
-        self.driver.back()
+       
         
 
     def save_issue(self, soup):
@@ -208,18 +216,19 @@ class Title():
         issue_path = os.path.join(self.title_dir, f"{issue_title}.json")
         with open(issue_path, "w") as f:
             json.dump(self.story_object, f)
-
+'''
 def save_one(url,title_dir):
-    avengers = Avengers()
-
     url = "https://uncannyxmen.net/comics/issue/uncanny-avengers-annual-1st-series-1"
     title_dir = "C:\\Users\\Bilbo\\Documents\\dev\\python\\marvel_universe\\data\\avengers\\issue_summaries\\UncannyAvengers(1stseries)"
     soup = BeautifulSoup(requests.get(url).content, "html.parser")
     title = Title(soup, title_dir)
     title.save_issue(soup)
+'''
+    
 def main():
+    output_json_path = "C:\\Temp\\issue_summaries_xmen"
     url = "https://uncannyxmen.net"
-    avengers = Avengers(url, "issue_summaries_xmen")
+    avengers = Avengers(url, output_json_path)
     avengers.get_summaries()
 
 
